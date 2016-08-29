8f521ffddf7979f585a994bb4a87e6e2214f2e6b
Fabio Soares <fabioars@live.com>
2016-08-28 23:05:28 -0300

    Colocando barra fixa na sessão 'o quê'

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -2,40 +2,56 @@
  * Created by Fabio Soares on 27/08/2016.
  */
 
+function lock(element){
+    element.style.position = "fixed";
+    element.style.top = '40px';
+    var a = document.getElementById("whatContent");
+    a.style.marginTop = '72px';
+}
+function unlock(element){
+    element.style.position = "relative";
+    element.style.top = '0';
+    var a = document.getElementById("whatContent");
+    a.style.marginTop = '0';
+}
+function ajustTopbar(elements){
+    if(window.pageYOffset > 30){
+        for(let i = 0; i < elements.length; i++){
+            elements[i].className = 'column navItem smaller';
+        }
+    }else{
+        for(let i = 0; i < elements.length; i++) {
+            elements[i].className = 'column navItem';
+        }
+    }
+}
 window.addEventListener('load', function(){
     var elements = document.getElementsByClassName('navItem');
     var topicElements = document.getElementsByClassName('sectionHeader');
 
-
+    ajustTopbar(elements);
     window.addEventListener('scroll', function(){
-        if(window.pageYOffset > 30){
-            for(var i = 0; i < elements.length; i++){
-                elements[i].className = 'column navItem smaller';
-            }
-        }else{
-            for (var i = 0; i < elements.length; i++) {
-                elements[i].className = 'column navItem';
-            }
-        }
+        ajustTopbar(elements);
 
-        if(window.pageYOffset+44 >= topicElements[0].offsetTop && window.pageYOffset+44 < topicElements[1].offsetTop){
-            var whatElement = document.querySelector('.sectionHeader.what');
-            whatElement.style.position = "fixed";
-            whatElement.style.top = '42px';
-            whatElement.style.width = "100%";
+        unlock(topicElements[0]);
+
+        if(window.pageYOffset+40 >= topicElements[0].offsetTop && window.pageYOffset+40 < topicElements[1].offsetTop){
+            lock(topicElements[0]);
             console.log("SCROLL: O QUÊ");
-        }else if(window.pageYOffset+44 >= topicElements[1].offsetTop && window.pageYOffset+44 < topicElements[2].offsetTop){
-
+        }else if(window.pageYOffset+40 >= topicElements[1].offsetTop && window.pageYOffset+40 < topicElements[2].offsetTop){
+            unlock(topicElements[0]);
             console.log("SCROLL: QUEM");
-        }else if(window.pageYOffset+44 >= topicElements[2].offsetTop && window.pageYOffset+44 < topicElements[3].offsetTop){
+        }else if(window.pageYOffset+40 >= topicElements[2].offsetTop && window.pageYOffset+40 < topicElements[3].offsetTop){
 
             console.log("SCROLL: QUANDO");
-        }else if(window.pageYOffset+44 >= topicElements[3].offsetTop && window.pageYOffset+44 < topicElements[4].offsetTop){
+        }else if(window.pageYOffset+40 >= topicElements[3].offsetTop && window.pageYOffset+40 < topicElements[4].offsetTop){
 
             console.log("SCROLL: ONDE");
-        }else if(window.pageYOffset+44 >= topicElements[4].offsetTop){
+        }else if(window.pageYOffset+40 >= topicElements[4].offsetTop){
 
             console.log("SCROLL: QUANTO");
+        }else{
+            unlock(topicElements[0]);
         }
 
 
