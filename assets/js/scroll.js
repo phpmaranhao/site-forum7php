bb8647a55ae10d6d5b4820a9085606b0dc32bf93
Fabio Soares <fabioars@live.com>
2016-09-06 09:09:56 -0300

    Organizando o código e deixando o menu do topo responsivo

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -2,33 +2,6 @@
  * Created by Fabio Soares on 27/08/2016.
  */
 
-function lock(element){
-    element.style.position = "fixed";
-    element.style.top = '40px';
-}
-function ajustBrother(element){
-    element.style.paddingTop = "62px";
-}
-function resetBrother(element){
-    element.style.paddingTop = "0";
-}
-function resetAllBrothers(elements){
-    for(let i = 0; i < elements.length; i++){
-        resetBrother(elements[i]);
-    }
-}
-
-function unlock(element){
-    element.style.position = "relative";
-    element.style.top = '0';
-}
-
-function unlockAll(elements){
-    for(let i = 0; i < elements.length; i++){
-        unlock(elements[i]);
-    }
-}
-
 function addClass(element, stringClass){
     let classes = element.className;
 
@@ -67,53 +40,31 @@ window.addEventListener('load', function(){
     var navBar = document.getElementById('navBar');
     var elements = document.getElementsByClassName('navItem');
     var topicElements = document.getElementsByClassName('sectionHeader');
-    var topicBrothers = document.getElementsByClassName('content');
 
     ajustTopbar(elements);
     window.addEventListener('scroll', function(){
         ajustTopbar(elements);
-
-        // unlockAll(topicElements);
-        // resetAllBrothers(topicBrothers);
         removeClass(navBar, "what");
         removeClass(navBar, "who");
         removeClass(navBar, "when");
         removeClass(navBar, "where");
         removeClass(navBar, "howMuch");
-
         if(window.pageYOffset+40 >= topicElements[0].offsetTop && window.pageYOffset+40 < topicElements[1].offsetTop){
-            // lock(topicElements[0]);
-            // ajustBrother(topicBrothers[0]);
             addClass(navBar, "what");
-            console.log("SCROLL: O QUÊ");
         }
         if(window.pageYOffset+40 >= topicElements[1].offsetTop && window.pageYOffset+40 < topicElements[2].offsetTop){
-            // lock(topicElements[1]);
-            // ajustBrother(topicBrothers[1]);
             addClass(navBar, "who");
-            console.log("SCROLL: QUEM");
         }
         if(window.pageYOffset+40 >= topicElements[2].offsetTop && window.pageYOffset+40 < topicElements[3].offsetTop){
-            // lock(topicElements[2]);
-            // ajustBrother(topicBrothers[2]);
             addClass(navBar, "when");
-            console.log("SCROLL: QUANDO");
         }
         if(window.pageYOffset+40 >= topicElements[3].offsetTop && window.pageYOffset+40 < topicElements[4].offsetTop){
-            // lock(topicElements[3]);
-            // ajustBrother(topicBrothers[3]);
             addClass(navBar, "where");
-            console.log("SCROLL: ONDE");
         }
         if(window.pageYOffset+40 >= topicElements[4].offsetTop){
-            // lock(topicElements[4]);
-            // ajustBrother(topicBrothers[4]);
             addClass(navBar, "where");
-            console.log("SCROLL: QUANTO");
         }
-
     });
-
     window.addEventListener('resize', function(){
         topicElements = document.getElementsByClassName('sectionHeader');
     });
