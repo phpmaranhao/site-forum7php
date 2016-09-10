d05f18cfe1f688151c25d510e0b967b6b6da3e9d
atmosmaciel <atmos.contato@gmail.com>
2016-09-10 00:18:45 -0300

    melhoria no codio do HomeController; cricao de funcoes e rotas de Whocontroller

--- src/Controller/WhoController.php
+++ src/Controller/WhoController.php
@@ -16,8 +16,78 @@ class WhoController
         $this->app = $app;
     }
 
-    public function PalestranteA()
+    public function speaker_A()
     {
         return $this->app['twig']->render('/who/palestranteA.twig');
+    }
+
+    public function speaker_B()
+    {
+        return "Sem View Para este Palestrante ainda";
+    }
+
+    public function speaker_C()
+    {
+        return "Sem View Para este Palestrante ainda";
+    }
+
+    public function speaker_D()
+    {
+        return "Sem View Para este Palestrante ainda";
+    }
+
+    public function speaker_E()
+    {
+        return "Sem View Para este Palestrante ainda";
+    }
+
+    public function speaker_F()
+    {
+        return "Sem View Para este Palestrante ainda";
+    }
+
+    public function speaker_G()
+    {
+        return "Sem View Para este Palestrante ainda";
+    }
+
+    public function speaker_H()
+    {
+        return "Sem View Para este Palestrante ainda";
+    }
+
+    public function speaker_I()
+    {
+        return "Sem View Para este Palestrante ainda";
+    }
+
+    public function speaker_J()
+    {
+        return "Sem View Para este Palestrante ainda";
+    }
+
+    public function speaker_L()
+    {
+        return "Sem View Para este Palestrante ainda";
+    }
+
+    public function speaker_M()
+    {
+        return "Sem View Para este Palestrante ainda";
+    }
+
+    public function speaker_N()
+    {
+        return "Sem View Para este Palestrante ainda";
+    }
+
+    public function speaker_O()
+    {
+        return "Sem View Para este Palestrante ainda";
+    }
+
+    public function speaker_P()
+    {
+        return "Sem View Para este Palestrante ainda";
     }
 }
