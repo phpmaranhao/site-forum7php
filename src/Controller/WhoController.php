e35e4f90d27d8ea14dccb089d1256b8a7d34ce95
Fabio Soares <fabioars@live.com>
2016-09-16 11:02:44 -0300

    Alterações nas páginas dos palestrantes

--- src/Controller/WhoController.php
+++ src/Controller/WhoController.php
@@ -15,10 +15,6 @@ class WhoController{
     }
 
     public function speaker_A(){
-        return $this->app['twig']->render('/who/speaker.twig');
-    }
-
-    public function speaker_B(){
         $content                     = array();
         $content['name']             = "Atmos Maciel";
         $content['photo']            = "rocket-night.jpg";
@@ -27,33 +23,79 @@ class WhoController{
         $content['slidePhoto']       = "rocket-night.jpg";
         $content['slideName']        = "Foguetes e a Nasa";
         $content['slideDescription'] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis quam nec dui faucibus, et dapibus erat volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis quam nec dui faucibus, et dapibus erat volutpat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis quam nec dui faucibus, et dapibus erat volutpat.";
-        $content['slideLink']        = "http://speakerdeck.com/fsoares/slide";
+        $content['slideLink']        = "http://speakerdeck.com/atmos/slide";
+
+        return $this->app['twig']->render('/who/speaker.twig', $content);
+    }
+
+    public function speaker_B(){
+        $content                     = array();
+        $content['name']             = "Evaldo Barbosa";
+        $content['photo']            = "rocket-night.jpg";
+        $content['description']      = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis quam nec dui faucibus, et dapibus erat volutpat. Praesent id enim justo. Ut lacinia rutrum ex, a aliquam enim dignissim at. Curabitur ut orci sit amet arcu cursus accumsan. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc sit amet tortor justo. Duis feugiat tristique arcu, vel semper lectus fermentum facilisis. Sed ac magna a ipsum ornare pulvinar.";
+        $content['type']             = "Palestra";
+        $content['slidePhoto']       = "rocket-night.jpg";
+        $content['slideName']        = "Foguetes e a Nasa";
+        $content['slideDescription'] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis quam nec dui faucibus, et dapibus erat volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis quam nec dui faucibus, et dapibus erat volutpat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis quam nec dui faucibus, et dapibus erat volutpat.";
+        $content['slideLink']        = "http://speakerdeck.com/evaldo/slide";
 
         return $this->app['twig']->render('/who/speaker.twig', $content);
     }
 
     public function speaker_C(){
-        return "Ainda não tem nada pra ser mostrado aqui...
-        que triste ... estou me sentindo sozinho e isolado...";
+        $content                     = array();
+        $content['name']             = "Fabio Soares";
+        $content['photo']            = "rocket-night.jpg";
+        $content['description']      = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis quam nec dui faucibus, et dapibus erat volutpat. Praesent id enim justo. Ut lacinia rutrum ex, a aliquam enim dignissim at. Curabitur ut orci sit amet arcu cursus accumsan. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc sit amet tortor justo. Duis feugiat tristique arcu, vel semper lectus fermentum facilisis. Sed ac magna a ipsum ornare pulvinar.";
+        $content['type']             = "Palestra";
+        $content['slidePhoto']       = "rocket-night.jpg";
+        $content['slideName']        = "Foguetes e a Nasa";
+        $content['slideDescription'] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis quam nec dui faucibus, et dapibus erat volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis quam nec dui faucibus, et dapibus erat volutpat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis quam nec dui faucibus, et dapibus erat volutpat.";
+        $content['slideLink']        = "https://speakerdeck.com/1fabiosoares";
+
+        return $this->app['twig']->render('/who/speaker.twig', $content);
     }
 
     public function speaker_D(){
-        return "Eu desço dessa solidão
-                Espalho coisas
-                Sobre um Chão de Giz
-                Há meros devaneios tolos
-                A me torturar
-                Fotografias recortadas
-                Em jornais de folhas
-                Amiúde!";
+        $content                     = array();
+        $content['name']             = "Nanderson Castro";
+        $content['photo']            = "/speakers/nanderson-castro.jpg";
+        $content['description']      = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis quam nec dui faucibus, et dapibus erat volutpat. Praesent id enim justo. Ut lacinia rutrum ex, a aliquam enim dignissim at. Curabitur ut orci sit amet arcu cursus accumsan. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc sit amet tortor justo. Duis feugiat tristique arcu, vel semper lectus fermentum facilisis. Sed ac magna a ipsum ornare pulvinar.";
+        $content['type']             = "Palestra";
+        $content['slidePhoto']       = "rocket-night.jpg";
+        $content['slideName']        = "Foguetes e a Nasa";
+        $content['slideDescription'] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis quam nec dui faucibus, et dapibus erat volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis quam nec dui faucibus, et dapibus erat volutpat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis quam nec dui faucibus, et dapibus erat volutpat.";
+        $content['slideLink']        = "http://speakerdeck.com/nanderson/slide";
+
+        return $this->app['twig']->render('/who/speaker.twig', $content);
     }
 
     public function speaker_E(){
-        return "Sem View Para este Palestrante ainda";
+        $content                     = array();
+        $content['name']             = "Ricardo Coelho";
+        $content['photo']            = "/speakers/ricardo-coelho.jpg";
+        $content['description']      = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis quam nec dui faucibus, et dapibus erat volutpat. Praesent id enim justo. Ut lacinia rutrum ex, a aliquam enim dignissim at. Curabitur ut orci sit amet arcu cursus accumsan. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc sit amet tortor justo. Duis feugiat tristique arcu, vel semper lectus fermentum facilisis. Sed ac magna a ipsum ornare pulvinar.";
+        $content['type']             = "Palestra";
+        $content['slidePhoto']       = "rocket-night.jpg";
+        $content['slideName']        = "Foguetes e a Nasa";
+        $content['slideDescription'] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis quam nec dui faucibus, et dapibus erat volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis quam nec dui faucibus, et dapibus erat volutpat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis quam nec dui faucibus, et dapibus erat volutpat.";
+        $content['slideLink']        = "https://speakerdeck.com/ramcoelho";
+
+        return $this->app['twig']->render('/who/speaker.twig', $content);
     }
 
     public function speaker_F(){
-        return "Sem View Para este Palestrante ainda";
+        $content                     = array();
+        $content['name']             = "William Mano";
+        $content['photo']            = "rocket-night.jpg";
+        $content['description']      = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis quam nec dui faucibus, et dapibus erat volutpat. Praesent id enim justo. Ut lacinia rutrum ex, a aliquam enim dignissim at. Curabitur ut orci sit amet arcu cursus accumsan. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc sit amet tortor justo. Duis feugiat tristique arcu, vel semper lectus fermentum facilisis. Sed ac magna a ipsum ornare pulvinar.";
+        $content['type']             = "Palestra";
+        $content['slidePhoto']       = "rocket-night.jpg";
+        $content['slideName']        = "Foguetes e a Nasa";
+        $content['slideDescription'] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis quam nec dui faucibus, et dapibus erat volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis quam nec dui faucibus, et dapibus erat volutpat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis quam nec dui faucibus, et dapibus erat volutpat.";
+        $content['slideLink']        = "https://speakerdeck.com/mano";
+
+        return $this->app['twig']->render('/who/speaker.twig', $content);
     }
 
     public function speaker_G(){
